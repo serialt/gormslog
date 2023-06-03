2e8c69d7219de099b7024a6e9003ac331591dbd0
serialt <tserialt@gmail.com>
2023-06-03 10:52:08 +0800

    feat: add

--- gormslog.go
+++ gormslog.go
@@ -9,47 +9,77 @@ import (
 	"golang.org/x/exp/slog"
 	"gorm.io/gorm"
 	"gorm.io/gorm/logger"
+	gormlogger "gorm.io/gorm/logger"
 )
 
-type Handler struct {
-	Debug                 bool
-	SkipErrRecordNotFound bool
-	SlowThreshold         time.Duration
+type Logger struct {
+	LogLevel                  gormlogger.LogLevel
+	SlowThreshold             time.Duration
+	SkipCallerLookup          bool
+	IgnoreRecordNotFoundError bool
 }
 
-var _ logger.Interface = (*Handler)(nil)
-
-func (h *Handler) LogMode(logger.LogLevel) logger.Interface {
-	return h
+func New(log *slog.Logger) Logger {
+	// slog.SetDefault(log)
+	return Logger{
+		LogLevel:                  gormlogger.Warn,
+		SlowThreshold:             100 * time.Millisecond,
+		SkipCallerLookup:          false,
+		IgnoreRecordNotFoundError: false,
+	}
 }
 
-func (h *Handler) Info(ctx context.Context, str string, args ...interface{}) {
+func (l Logger) SetAsDefault() {
+	gormlogger.Default = l
+}
+
+func (l Logger) LogMode(level gormlogger.LogLevel) logger.Interface {
+	return Logger{
+		LogLevel:                  level,
+		SlowThreshold:             l.SlowThreshold,
+		SkipCallerLookup:          l.SkipCallerLookup,
+		IgnoreRecordNotFoundError: l.IgnoreRecordNotFoundError,
+	}
+
+}
+
+func (l Logger) Info(ctx context.Context, str string, args ...interface{}) {
+	if l.LogLevel < gormlogger.Info {
+		return
+	}
 	slog.InfoCtx(ctx, str, args...)
 }
 
-func (h *Handler) Warn(ctx context.Context, str string, args ...interface{}) {
+func (l Logger) Warn(ctx context.Context, str string, args ...interface{}) {
+	if l.LogLevel < gormlogger.Warn {
+		return
+	}
 	slog.WarnCtx(ctx, str, args...)
 }
 
-func (h *Handler) Error(ctx context.Context, str string, args ...interface{}) {
+func (l Logger) Error(ctx context.Context, str string, args ...interface{}) {
+	if l.LogLevel < gormlogger.Error {
+		return
+	}
 	slog.ErrorCtx(ctx, str, args...)
 }
 
-func (h *Handler) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
+func (l Logger) Trace(ctx context.Context, begin time.Time, fc func() (sql string, rowsAffected int64), err error) {
+	if l.LogLevel <= 0 {
+		return
+	}
 	elapsed := time.Since(begin)
-	sql, _ := fc()
 
-	if err != nil && !(errors.Is(err, gorm.ErrRecordNotFound) && h.SkipErrRecordNotFound) {
-		slog.ErrorCtx(ctx, fmt.Sprintf("%s [%s]", sql, elapsed), slog.String("error", err.Error()))
-		return
-	}
+	switch {
+	case err != nil && l.LogLevel >= gormlogger.Error && (!l.IgnoreRecordNotFoundError || !errors.Is(err, gorm.ErrRecordNotFound)):
+		sql, rows := fc()
+		slog.ErrorCtx(ctx, fmt.Sprint(err), "elapsed", elapsed, "rows", rows, "sql", sql)
+	case l.SlowThreshold != 0 && elapsed > l.SlowThreshold && l.LogLevel >= gormlogger.Warn:
+		sql, rows := fc()
+		slog.WarnCtx(ctx, fmt.Sprint(err), "elapsed", elapsed, "rows", rows, "sql", sql)
 
-	if h.SlowThreshold != 0 && elapsed > h.SlowThreshold {
-		slog.WarnCtx(ctx, fmt.Sprintf("%s [%s]", sql, elapsed))
-		return
-	}
-
-	if h.Debug {
-		slog.DebugCtx(ctx, fmt.Sprintf("%s [%s]", sql, elapsed))
+	case l.LogLevel >= gormlogger.Info:
+		sql, rows := fc()
+		slog.DebugCtx(ctx, fmt.Sprint(err), "elapsed", elapsed, "rows", rows, "sql", sql)
 	}
 }
